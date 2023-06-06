e6e2252493ec3b6c15a7fdae6b9676a39e2b2040
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-06-06 23:52:57 +0700

    Initial Commit

---
+++ BankSimulation/BankSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankSimulationApp.swift
+//  BankSimulation
+//
+//  Created by Steven Wijaya on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BankSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
