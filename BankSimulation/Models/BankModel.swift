4b750edca250086493c01a7f0df63e192080296a
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-06-07 00:07:54 +0700

    Added model and viewmodel empty file

---
+++ BankSimulation/Models/BankModel.swift
@@ -0,0 +1,8 @@
+//
+//  BankModel.swift
+//  BankSimulation
+//
+//  Created by Steven Wijaya on 07.06.2023.
+//
+
+import Foundation
---
+++ BankSimulation/ViewModels/BSViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BSViewModel.swift
+//  BankSimulation
+//
+//  Created by Steven Wijaya on 07.06.2023.
+//
+
+import Foundation
